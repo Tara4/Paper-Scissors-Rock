739cb5a517c0bbacd18a7ac60ca857aa1901b60c
Tara4 <tararlee2000@gmail.com>
2021-03-13 12:36:21 +1300

    Add Project

---
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CPUPictureBox = new System.Windows.Forms.PictureBox();
+            this.playerPictureBox = new System.Windows.Forms.PictureBox();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scissorsButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.CPUScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.playerScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CPUPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CPUPictureBox
+            // 
+            this.CPUPictureBox.Location = new System.Drawing.Point(626, 33);
+            this.CPUPictureBox.Name = "CPUPictureBox";
+            this.CPUPictureBox.Size = new System.Drawing.Size(430, 302);
+            this.CPUPictureBox.TabIndex = 24;
+            this.CPUPictureBox.TabStop = false;
+            // 
+            // playerPictureBox
+            // 
+            this.playerPictureBox.Location = new System.Drawing.Point(153, 33);
+            this.playerPictureBox.Name = "playerPictureBox";
+            this.playerPictureBox.Size = new System.Drawing.Size(458, 302);
+            this.playerPictureBox.TabIndex = 23;
+            this.playerPictureBox.TabStop = false;
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(439, 393);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(239, 38);
+            this.restartButton.TabIndex = 22;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(452, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 25);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Score 5 points to win";
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scissorsButton.Location = new System.Drawing.Point(25, 205);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(93, 38);
+            this.scissorsButton.TabIndex = 20;
+            this.scissorsButton.Text = "Scissors";
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            this.scissorsButton.Click += new System.EventHandler(this.scissorsButton_Click);
+            // 
+            // paperButton
+            // 
+            this.paperButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paperButton.Location = new System.Drawing.Point(25, 161);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(93, 38);
+            this.paperButton.TabIndex = 19;
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = true;
+            this.paperButton.Click += new System.EventHandler(this.paperButton_Click);
+            // 
+            // rockButton
+            // 
+            this.rockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rockButton.Location = new System.Drawing.Point(25, 117);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(93, 38);
+            this.rockButton.TabIndex = 18;
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.rockButton_Click);
+            // 
+            // CPUScore
+            // 
+            this.CPUScore.AutoSize = true;
+            this.CPUScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPUScore.ForeColor = System.Drawing.Color.Red;
+            this.CPUScore.Location = new System.Drawing.Point(1109, 47);
+            this.CPUScore.Name = "CPUScore";
+            this.CPUScore.Size = new System.Drawing.Size(33, 36);
+            this.CPUScore.TabIndex = 17;
+            this.CPUScore.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1095, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Score";
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScore.ForeColor = System.Drawing.Color.Blue;
+            this.playerScore.Location = new System.Drawing.Point(34, 47);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(33, 36);
+            this.playerScore.TabIndex = 15;
+            this.playerScore.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 450);
+            this.Controls.Add(this.CPUPictureBox);
+            this.Controls.Add(this.playerPictureBox);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scissorsButton);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.CPUScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.playerScore);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CPUPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CPUPictureBox;
+        private System.Windows.Forms.PictureBox playerPictureBox;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button scissorsButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Label CPUScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int PlayerScore = 0;
+        public int CPUscore = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playerPictureBox.Image = Image.FromFile("PlayerShake.gif");
+            CPUPictureBox.Image = Image.FromFile("CPUShake.gif");
+        }
+
+        private void rockButton_Click(object sender, EventArgs e)
+        {
+            playerPictureBox.Image = Image.FromFile("PlayerRock.gif");
+
+            Random r = new Random();
+            int computerChoice = r.Next(3);
+
+
+            //Rock
+            if (computerChoice == 0)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPURock.gif");
+                MessageBox.Show("Tie");
+            }
+
+            //Paper
+            else if (computerChoice == 1)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPUPaper.gif");
+                MessageBox.Show("CPU Wins");
+                CPUscore = CPUscore + 1;
+            }
+
+            //Scissors
+            else if (computerChoice == 2)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPUScissors.gif");
+                MessageBox.Show("Player Wins");
+                PlayerScore = PlayerScore + 1;
+            }
+            playerPictureBox.Image = Image.FromFile("PlayerShake.gif");
+            CPUPictureBox.Image = Image.FromFile("CPUShake.gif");
+            playerScore.Text = PlayerScore.ToString();
+            CPUScore.Text = CPUscore.ToString();
+
+            if (PlayerScore == 5)
+            {
+                MessageBox.Show("Player Wins the Game!");
+                PlayerScore = 0;
+                CPUscore = 0;
+                playerScore.Text = PlayerScore.ToString();
+                CPUScore.Text = CPUscore.ToString();
+            }
+
+            else if (CPUscore == 5)
+            {
+                MessageBox.Show("CPU Wins the Game!");
+                PlayerScore = 0;
+                CPUscore = 0;
+                playerScore.Text = PlayerScore.ToString();
+                CPUScore.Text = CPUscore.ToString();
+            }
+        }
+
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+            playerPictureBox.Image = Image.FromFile("PlayerPaper.gif");
+
+            Random r = new Random();
+            int computerChoice = r.Next(3);
+
+            //Rock
+            if (computerChoice == 0)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPURock.gif");
+                MessageBox.Show("Player Wins");
+                PlayerScore = PlayerScore + 1;
+            }
+
+            //Paper
+            else if (computerChoice == 1)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPUPaper.gif");
+                MessageBox.Show("Tie");
+            }
+
+            //Scissors
+            else if (computerChoice == 2)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPUScissors.gif");
+                MessageBox.Show("CPU Wins");
+                CPUscore = CPUscore + 1;
+            }
+            playerPictureBox.Image = Image.FromFile("PlayerShake.gif");
+            CPUPictureBox.Image = Image.FromFile("CPUShake.gif");
+            playerScore.Text = PlayerScore.ToString();
+            CPUScore.Text = CPUscore.ToString();
+
+            if (PlayerScore == 5)
+            {
+                MessageBox.Show("Player Wins the Game!");
+                PlayerScore = 0;
+                CPUscore = 0;
+                playerScore.Text = PlayerScore.ToString();
+                CPUScore.Text = CPUscore.ToString();
+            }
+
+            else if (CPUscore == 5)
+            {
+                MessageBox.Show("CPU Wins the Game!");
+                PlayerScore = 0;
+                CPUscore = 0;
+                playerScore.Text = PlayerScore.ToString();
+                CPUScore.Text = CPUscore.ToString();
+            }
+        }
+
+        private void scissorsButton_Click(object sender, EventArgs e)
+        {
+            playerPictureBox.Image = Image.FromFile("PlayerScissors.gif");
+
+            Random r = new Random();
+            int computerChoice = r.Next(3);
+
+            //Rock
+            if (computerChoice == 0)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPURock.gif");
+                MessageBox.Show("CPU Wins");
+                CPUscore = CPUscore + 1;
+            }
+
+            //Paper
+            else if (computerChoice == 1)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPUPaper.gif");
+                MessageBox.Show("Player Wins");
+                PlayerScore = PlayerScore + 1;
+            }
+
+            //Scissors
+            else if (computerChoice == 2)
+            {
+                CPUPictureBox.Image = Image.FromFile("CPUScissors.gif");
+                MessageBox.Show("Tie");
+            }
+            playerPictureBox.Image = Image.FromFile("PlayerShake.gif");
+            CPUPictureBox.Image = Image.FromFile("CPUShake.gif");
+            playerScore.Text = PlayerScore.ToString();
+            CPUScore.Text = CPUscore.ToString();
+
+            if (PlayerScore == 5)
+            {
+                MessageBox.Show("Player Wins the Game!");
+                PlayerScore = 0;
+                CPUscore = 0;
+                playerScore.Text = PlayerScore.ToString();
+                CPUScore.Text = CPUscore.ToString();
+            }
+
+            else if (CPUscore == 5)
+            {
+                MessageBox.Show("CPU Wins the Game!");
+                PlayerScore = 0;
+                CPUscore = 0;
+                playerScore.Text = PlayerScore.ToString();
+                CPUScore.Text = CPUscore.ToString();
+            }
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Game Reset");
+            PlayerScore = 0;
+            CPUscore = 0;
+            playerScore.Text = PlayerScore.ToString();
+            CPUScore.Text = CPUscore.ToString();
+        }
+    }
+}
